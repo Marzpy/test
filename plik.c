f7436b584dca9956dfb4e918aca46a44f88ebb12
Marzpy <marcinloin09@gmail.com>
2025-11-10 21:11:52 +0100

    po merge

--- plik.c
+++ plik.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 git init  
 hello zmiany
 sci
@@ -7,8 +6,3 @@ zmiany w kodzie nowe są
 
 nwoe zmiany na feat1
 kolejne zmiany na feat1 commit
-=======
-git init
-
-zmiany na mainie 
->>>>>>> 56f5819f42a11e39456aaf176f09bbacf49e754c
